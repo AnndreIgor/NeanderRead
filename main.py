b2e2ec2f2490852c3d24bd447f67aa26c5e8478b
AnndreIgor <ip.andre@hotmail.com>
2024-04-13 12:04:08 -0300

    Criacao do projeto

---
+++ main.py
@@ -0,0 +1,79 @@
+import numpy as np
+import os
+
+instruction_set = {
+    'NOP':  0,
+    'STA': 16,
+    'LDA': 32,
+    'ADD': 48,
+    'OR':  64,
+    'AND': 80,
+    'NOT': 96,
+    'JMP': 128,
+    'JN':  144,
+    'JZ':  160,
+    'HLT': 240
+}
+
+variables = {
+    'MENOS_UM': 136,
+    'AUX_A2': 135,
+    'AUX_A': 132,
+    'AUX_N': 133,
+    'ZERO': 134,
+    'R1': 130,
+    'A': 128,
+    'N': 129,
+    'X': 131
+}
+
+
+def txt_to_mem(text_file: str, instruction_set: dict, variables: dict):
+    with open(text_file, 'r') as f:
+        text = f.read()
+
+    text = replace_intructions(text, instruction_set)
+    text = replace_intructions(text, variables)
+
+    result = format_mem_file(text)
+    save_mem_file(result, text_file)
+
+    return result
+
+def replace_intructions(text: str, instruction_set: dict) -> str:
+    for key, value in instruction_set.items():
+        text = text.replace(key, str(value))
+
+    return text
+
+def replace_variables(text: str, variables: dict) -> str:
+    for key, value in variables.items():
+        text = text.replace(key, str(value))
+
+    return text
+
+def format_mem_file(text: str):
+    instructions = text.split()
+
+    mem = np.zeros(516)
+
+    # carregar as instruções default do neander
+    instructions_neander_default = ['3', '78', '68', '82']
+    for index, instruction in enumerate(instructions_neander_default):
+        mem[index] = instruction
+
+    i = 4
+    for instruction in instructions:
+        mem[i] = instruction
+        i += 2
+
+    return mem.astype('uint8')
+
+def save_mem_file(array: np.array, text_file: str):
+    output_name, _ = os.path.splitext(text_file)
+    array.tofile(output_name + '.mem')
+
+
+if __name__ == '__main__':
+    meu = txt_to_mem('multiplicacao.txt', instruction_set, variables)
+    print(meu)
